2997f487e057ef9f0736441bf033dc05726dede8
mizukili1962 <mizuki.li1962@gmail.com>
2026-05-25 09:35:07 +0900

    Update chengyuService.ts

--- src/services/chengyuService.ts
+++ src/services/chengyuService.ts
@@ -1,1 +1,14 @@
+import { db } from '../firebase';
+import { collection, getDocs } from 'firebase/firestore';
 
+export const fetchChengyuList = async () => {
+  const snap = await getDocs(
+    collection(db, 'masterData', 'chengyu', 'list')
+  );
+
+  return snap.docs.map(doc => ({
+    zh: doc.data().chinese,
+    py: doc.data().pinyin,
+    ja: doc.data().japanese,
+  }));
+};
