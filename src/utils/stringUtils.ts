ddcb6786d1c9d0ea810944bafc77aa54c19b4d1b
mizukili1962 <mizuki.li1962@gmail.com>
2026-05-28 09:50:53 +0900

    Update stringUtils.ts

--- src/utils/stringUtils.ts
+++ src/utils/stringUtils.ts
@@ -1,1 +1,31 @@
-
+// 文字列の類似度を計算（Levenshtein距離を使用）
+  const calculateSimilarity = (str1: string, str2: string): number => {
+    const len1 = str1.length;
+    const len2 = str2.length;
+    const matrix: number[][] = [];
+    
+    for (let i = 0; i <= len2; i++) {
+      matrix[i] = [i];
+    }
+    for (let j = 0; j <= len1; j++) {
+      matrix[0][j] = j;
+    }
+    
+    for (let i = 1; i <= len2; i++) {
+      for (let j = 1; j <= len1; j++) {
+        if (str2.charAt(i - 1) === str1.charAt(j - 1)) {
+          matrix[i][j] = matrix[i - 1][j - 1];
+        } else {
+          matrix[i][j] = Math.min(
+            matrix[i - 1][j - 1] + 1,
+            matrix[i][j - 1] + 1,
+            matrix[i - 1][j] + 1
+          );
+        }
+      }
+    }
+    
+    const distance = matrix[len2][len1];
+    const maxLen = Math.max(len1, len2);
+    return 1 - (distance / maxLen);
+  };
