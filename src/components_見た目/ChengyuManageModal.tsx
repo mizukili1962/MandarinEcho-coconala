f66ff456630877dc3dd46956005313be4deff38a
mizukili1962 <mizuki.li1962@gmail.com>
2026-05-31 06:13:52 +0900

    Update ChengyuManageModal.tsx

--- src/components_見た目/ChengyuManageModal.tsx
+++ src/components_見た目/ChengyuManageModal.tsx
@@ -1,23 +1,5 @@
-import type { Chengyu } from '../types';
-import React from 'react';
-
-type Props = {
-  allChengyuList: Chengyu[];
-  setAllChengyuList: React.Dispatch<React.SetStateAction<Chengyu[]>>;
-  setRandomChengyu: React.Dispatch<React.SetStateAction<Chengyu | null>>;
-  setIsChengyuManageModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
-  setIsChengyuModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
-  setEditingChengyu: React.Dispatch<React.SetStateAction<Chengyu | null>>;
-  setIsChengyuImportModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
-};
-
-const ChengyuManageModal = ({
-  
-  return (
-    <div>
-      ChengyuManageModal
-    </div>
-  );
+const ChengyuManageModal = () => {
+  return <div>ChengyuManageModal</div>;
 };
 
 export default ChengyuManageModal;
